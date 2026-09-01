8c41801f9874e5b51f8efe2b9f785b372280c2a0
nigma91-corvus <nigma91@gmail.com>
2026-09-01 12:36:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,7 @@ import numpy as np
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from supabase import create_clientimport base64
-from datetime import datetime, date
-import os
-import io
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 from supabase import create_client
-
 # -----------------------------------------------------------------------------
 # CONFIGURACIÓN DE PÁGINA
 # -----------------------------------------------------------------------------
