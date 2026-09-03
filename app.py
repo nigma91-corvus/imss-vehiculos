abd9ac474eeafc8fe5206f124d2fd983c87cea2c
nigma91-corvus <nigma91@gmail.com>
2026-09-03 10:17:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -338,12 +338,9 @@ with st.sidebar:
     )
 
   st.markdown(
-      "<div style='text-align: center; font-size: 11px; margin-bottom:"
-      " 10px;'><b>DIRECCIÓN DE ADMINISTRACIÓN</b><br><span"
-      f" style='font-size:9px; color:{COLORES_PANTONE['468']};'>Coordinación Técnica de Servicios"
-      " Generales</span></div>",
-      unsafe_allow_html=True,
-  )
+    f"<div style='text-align: center; font-size: 11px; margin-bottom: 10px;'><b>Coordinación Técnica de Servicios Generales</b><br><span style='font-size:9px; color:{COLORES_PANTONE[\"468\"]};'>División de Transportes y Operación</span></div>",
+    unsafe_allow_html=True,
+)
   st.markdown("---")
   st.markdown(
       "<p style='font-size: 12px; margin-bottom: 8px;'><b>SELECCIONAR"
