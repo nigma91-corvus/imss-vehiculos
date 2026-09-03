9d1b5fa4a2c71a825d1fb932f2304f6cf238b4b2
nigma91-corvus <nigma91@gmail.com>
2026-09-03 10:34:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ st.markdown(
     "División de Transportes y Operación</span></div>",
     unsafe_allow_html=True,
 )
-st.markdown("---")
+
   st.markdown("---")
   st.markdown(
       "<p style='font-size: 12px; margin-bottom: 8px;'><b>SELECCIONAR"
