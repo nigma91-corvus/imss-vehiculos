335a9bbfdd25da8d4a581c4010d0ce792b0ec8ea
nigma91-corvus <nigma91@gmail.com>
2026-09-09 11:01:39 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -392,13 +392,13 @@ with st.sidebar:
 
             /* 3. Reduce el espacio vertical específico entre un botón y otro */
             [data-testid="stSidebar"] .stButton {
-                margin-top: -150px !important;
-                margin-bottom: -110px !important;
+                margin-top: -130px !important;
+                margin-bottom: -130px !important;
             }
 
             /* 4. Sube el bloque completo de los Módulos del Sistema */
             [data-testid="stSidebar"] div.stRadio {
-                margin-top: -150px !important;
+                margin-top: -130px !important;
             }
         </style>
         """,
